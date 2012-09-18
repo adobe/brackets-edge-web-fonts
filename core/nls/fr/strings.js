49d16e4daf4b23c746505b1d13d1cf00622d721f
wALF Utility <walf@adobe.com>
2012-09-17 18:18:26 -0700

    Updated by ALF automation.

---
+++ core/nls/fr/strings.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+	"PRODUCT_NAME": "Adobe Edge Web Fonts",
+
+	"DIALOG_DONE": "Terminer",
+	"SEARCH_PLACEHOLDER": "Rechercher",
+	"BROWSE_FONTS_COMMAND_NAME": "[Ck] !é=Browse Edge Web Fonts=!",
+	"CODEHINT_BROWSE": "[Cl] !é=Browse Web Fonts...=!",
+	"GENERATE_INCLUDE_COMMAND_NAME": "[Cn] !é=Generate Edge Web Fonts Include Code=!",
+	"GENERATE_INCLUDE_TOOLTIP": "[Co] !é=Generate Edge Web Fonts Include Code=!",
+	"INCLUDE_INSTRUCTIONS_1": "[Cp] !é=Edge Web Fonts are loaded into the user's browser via JavaScript. To ensure that fonts are properly loaded, you must add a script tag to your HTML.=!",
+	"INCLUDE_INSTRUCTIONS_2": "[Cq] !é=Add the following script tag to all HTML files that reference this CSS file:=!",
+	"HOWTO_INSTRUCTIONS_1": "[C4] !é=The Adobe Edge Web Font service helps you add FREE and awesome fonts to your web project. Just specify the fonts you want to use in your CSS and we'll generate the required <script> tag for your HTML.=!",
+	"HOWTO_INSTRUCTIONS_2": "[C5] !é=When specifying a font-face property in a CSS document, select 'Browse Web Fonts' from the code completion drop-down to browse for free online web fonts.=!",
+	"HOWTO_INSTRUCTIONS_3": "[C7] !é=When your done, click the [ Wf ] icon in the top right to generate the required embed script.=!",
+	"HOWTO_INSTRUCTIONS_4": "[C8] !é=Paste the embed script into any HTML page to include the fonts.=!",
+	"BROWSER_HOWTO_HEADER": "[C2] !é=Next Step=!",
+	"BROWSER_HOWTO_INSTRUCTIONS": "[C3] !é=[[WHAT TO DO NEXT]]=!",
+    
+    
+    // Font classifications
+	"serif": "[C0] !é=Serif=!",
+	"sans-serif": "[Cy] !é=Sans-Serif=!",
+	"slab-serif": "[C1] !é=Slab-Serif=!",
+	"script": "Script",
+	"blackletter": "[Cs] !é=Blackletter=!",
+	"monospaced": "[Cw] !é=Monospaced=!",
+	"handmade": "[Cu] !é=Handmande=!",
+	"decorative": "[Ct] !é=Decorative=!",
+	"headings": "[Cv] !é=Headings=!",
+	"paragraphs": "[Cx] !é=Paragraphs=!"
+});
