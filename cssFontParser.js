70ca98c64d32015f405e096a52ab468949084ed9
Narciso Jaramillo <njaramil@adobe.com>
2012-09-19 11:37:12 -0700

    Added clarifying comment

--- cssFontParser.js
+++ cssFontParser.js
@@ -65,6 +65,10 @@ define(function (require, exports, module) {
                         isParsingFontList = false;
                     } else if (!(assumeCursorInvalid && userCursor.line === cursor.line &&
                                  userCursor.ch >= t.start && userCursor.ch <= t.end)) {
+                        // The if statement above makes it so that we don't add the font surrounding
+                        // the current cursor position to the completion list for autocompletion
+                        // (but we do for the <script> tag generation, because assumeCursorInvalid is
+                        // false in that case).
                         if (t.className === "number") {
                             fonts.push(t.string);
                         } else if (t.className === "string") {
